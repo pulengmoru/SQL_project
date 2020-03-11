b19ba772f19ac0eb823c9c9eb2d7b488b2231a78
pulengmoru <pulengmoru@gmail.com>
2020-03-11 17:15:03 +0200

    creating a notebook

--- data.sql
+++ data.sql
@@ -69,44 +69,5 @@ CREATE TABLE Products(
 
 INSERT INTO Products VALUES(1, 'Harley Davidson Chopper', 'This replica features working kickstand, front suspension, gear-shift lever', 150.75);
 INSERT INTO Products VALUES(2, 'Classic Car', 'Turnable front wheels, steering function', 550.75);
-INSERT INTO Products VALUES(3, 'Sports car', 'Turnable front wheels, steering function', 700.60)
+INSERT INTO Products VALUES(3, 'Sports car', 'Turnable front wheels', 'steering function', 700.60)
 
-#Part 2: Querying a database
-
-# 1.SELECT ALL records from table Customers.
-SELECT * FROM Customers;
-
-#2. SELECT records only from the name column in the Customers table.
-SELECT FirstName FROM Customers;
-
-#3. Show the name of the Customer whose CustomerID is 1.
-SELECT FirstName FROM Customers
-WHERE CustomerID = 1
-
-#4. UPDATE the record for CustomerID = 1 on the Customer table so that the name is “Lerato Mabitso”.
-
-#5. DELETE the record from the Customers table for customer 2 (CustomerID = 2).
-
-Select all unique statuses from the Orders table and get a count of the number of orders for each unique status.
-
-Return the MAXIMUM payment made on the PAYMENTS table.
-
-Select all customers from the “Customers” table, sorted by the “Country” column.
-
-Select all products with a price BETWEEN R100 and R600.
-
-Select all fields from “Customers” where country is “Germany” AND city is “Berlin”.
-
-Select all fields from “Customers” where city is “Cape Town” OR “Durban”.
-
-Select all records from Products where the Price is GREATER than R500.
-
-Return the sum of the Amounts on the Payments table.
-
-Count the number of shipped orders in the Orders table.
-
-Return the average price of all Products, in Rands and in Dollars (assume the exchange rate is R12 to the Dollar).
-
-Using INNER JOIN create a query that selects all Payments with Customer information.
-
-Select all products that have turnable front wheels.
